be85863689e95e653871a67aa13ab9cccb85ee8b
Muhammad Moiz <muhammadmoiz0087@gmail.com>
2020-07-16 00:40:43 +0500

    - reconfigure App

---
+++ App.tsx
@@ -0,0 +1,26 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {StyleSheet, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <Text>Hello, World</Text>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  Text: {
+    textAlign: 'center',
+  },
+});
+
+export default App;
